3f3bb1044cde5dde399974108a330cc1b18c0f17
YongHeeCh <alpacatnt@naver.com>
2024-07-10 18:20:30 +0900

    연산자 개요 산술연산자 실습

--- 02operator.py
+++ 02operator.py
@@ -5,7 +5,6 @@
 # 수식 -> 피연산자와 연산자로 구성
 # 피연산자 : 연산의 대상들을 의미
 
-
 # 산술연산자
 # 자료형 승격promotion
 
@@ -22,11 +21,6 @@ print(f'1분기 전체 매출 : {total}')
 seles = int(input('1분기매출액은?'))
 buys = int(input('1분기매입액은?'))
 profit = (seles) - (buys)
-#
-# print(f'''한빛전자에 1분기매출액은{seles}이고
-# 1분기매입액은{buys}이기에 한빛전자의 1분기
-# 수익액은{profit}입니다.
-# ''')
 
 print(f'1분기수익액 : {profit}원')
 # 방의 넓이 구하기
@@ -50,12 +44,10 @@ bmi = int(weight / (height * height))
 
 print(f'BMI : {bmi}')
 
-
 # 홀짝 게임
 coins = int(input('손안에 동전수를 입력하세요 :'))
 result = coins % 2
 print(f'{result}')
-
 
 # 빵 나누기
 
@@ -69,7 +61,6 @@ print(f'''
 남는 빵 갯수 : {mods}
 ''')
 
-
 # 전염병 예상 감염자 구하기
 # 1일차 -> 2명
 # 2일차 -> 4명
@@ -80,5 +71,55 @@ print(f'''
 infectors = 2 ** 30
 print(f'30일 이후 예상 감염자 수 : {infectors}')
 
+# 할당 연산자
+# 변수에 값을 대입하는 데 사용하는 연산자로 대입 연산자라고도 함
+# 할당 연산자를 사용하면 계산과 동시에 결과값을 변수에 넣을 수 있음
+# 할당 연산자의 종류는 다소 많지만
+# 연산자 기호가 직관적이기 때문에 쉽게 이해할 수 있음
+# 종류로는(=, +=, -+ ,*= , /= , %= , //=, **=)
 
+# 논리 연산자
+# 피연산자의 논리 자료형(True/False) 을 이용하는
+# 연산자로 and, or, not이 있음
+
+
+# 논리 연산자 단축식 평가
+# and 연산
+# 피연산자가 모두 True인 경우에만 결과가 True
+# 피연산자가 하나라도 False이면 결과는 False
+
+# or 연산
+# or은 '또는' 이라는 뜻으로, 피연산자 중 하나라도
+# True라면 결과값은 True
+
+# not 연산
+# not은 '부정'이라는 뜻으로, 피연산자의 현재 상태를 부정하는 연산자
+# 피연산자가 True이면 결과로 False를 출력하고
+# False이면 True를 출력
+
+# 삼항 연산자
+# 조건문을 한 줄로 표현할 수 있는 연산자
+# 참일때값 if 조건식 else 거짓일때값
+
+myScore = 75
+result = '합격!' if myScore >= 90 else '불합격!'
+
+print(result)
+
+# 복리 계산기
+Deposit = 5000000 ** 5
+print(f'5년만기 후 수령액 : {de}')
+
+
+# 범퍼카 탑승
+height = 150
+result = '탑승!' if height >= 120 else '탑승불가!'
+# 범퍼카 탑승 판별
+
+height = int(input('어린이탑승키'))
+result = '탑승' if height >= 120 and height < 170 else '탑승불가!'
+# 적자/흑자 판별
+
+income = 4000000
+expenditure = 3000000
 
