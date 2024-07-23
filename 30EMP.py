3d9db56d9c28a65ac1fca12d1fe0411f3ace0159
YongHeeCh <alpacatnt@naver.com>
2024-07-23 11:38:11 +0900

    사원등록 프로그램 - 수정 기능 추가

--- 30EMP.py
+++ 30EMP.py
@@ -25,10 +25,12 @@ while True:
 
     elif menu == '4':
         print('사원 데이터 수정')
-        pass
+        emp.modifyEmp()
+
     elif menu == '5':
         print('사원 데이터 삭제')
         emp.removeEmp()
+
     elif menu == '0':
         print('프로그램 종료')
         sys.exit(0)
